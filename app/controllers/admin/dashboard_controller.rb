78a0ee8bbd0710ef0fb8e69531a821586ccc2d80
bryanCN <bryan.chuinkam@gmail.com>
2023-06-28 16:05:56 -0400

    added details to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USERNAME"], password: ENV["HTTP_BASIC_PASSWORD"]
+
   def show
+    @num_products = Product.count
+    @num_categories = Category.count
   end
 end
